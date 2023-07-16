310747fbff3ac67cf96b8492e3e02fe2d0adeb60
henry642.LiG <69836425+henrylin642@users.noreply.github.com>
2023-07-16 22:52:18 +0800

    Update db_function.py

--- db_function.py
+++ db_function.py
@@ -120,6 +120,8 @@ def userdata_arrange(df):
 def upload(df,selected_db,uploaded_file):
     filename =  "data/" + df[df['db'] == selected_db]['filename'].values[0]
     st.write(filename)
+    df_test = pd.read_csv(filename)
+    st.write(df_test)
     if uploaded_file is not None:
         df_upload = pd.read_csv(uploaded_file, encoding="utf-8-sig")
         newfilename = uploaded_file.name
