df32e16abd5aa9f9e2b23ae05fb69cdc64717455
henry642.LiG <69836425+henrylin642@users.noreply.github.com>
2023-07-19 07:26:05 +0800

    Update db_main.py

--- db_main.py
+++ db_main.py
@@ -238,7 +238,6 @@ def main():
     
     with st.expander("各專案近30日掃描量"):
         st.dataframe(df_30day)
-        st.write(df_30day_trimmed.iloc[-1])
     
     #%% 展示資料集-By城市數據  ============================================================================= ##
     ## backed
