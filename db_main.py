a182dbe7b8df944909e01e521300449e00889501
henry642.LiG <69836425+henrylin642@users.noreply.github.com>
2023-07-19 07:19:34 +0800

    Update db_main.py

--- db_main.py
+++ db_main.py
@@ -234,7 +234,7 @@ def main():
     df_30day = df_30day.reindex(columns=df_30day.columns[::-1])
     # 計算第三欄以後所有數據的平均值
     average_values = df_30day.iloc[:, 1:].mean(axis=1).round(1)
-    df_30day.insert(1, '平均值', average_values)
+    df_30day.insert(1, '平均值', average_values.round(1))
     
     with st.expander("各專案近30日掃描量"):
         st.dataframe(df_30day)
