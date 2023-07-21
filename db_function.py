b7889bcc48c4cc0c200ada9b6f58a592ee4a12dc
henry642.LiG <69836425+henrylin642@users.noreply.github.com>
2023-07-22 05:58:52 +0800

    Update db_function.py

--- db_function.py
+++ db_function.py
@@ -110,7 +110,7 @@ def userdata_arrange(df):
     new_df['Created_at'] = pd.to_datetime(new_df['Created_at'], format='%Y年%m月%d日 %H:%M')
 
     # 将日期时间格式转换为一般的时间字符串格式
-    new_df['Created_at'] = new_df['Created_at'].dt.strftime('%Y-%m-%d %H:%M')
+    #new_df['Created_at'] = new_df['Created_at'].dt.strftime('%Y-%m-%d %H:%M')
     
     date_string = date.today().strftime('%m%d')
     new_filename = "data/userdata" +"_" + date_string + '.csv'
